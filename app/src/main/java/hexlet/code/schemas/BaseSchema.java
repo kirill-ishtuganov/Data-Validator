dc9edb346407bb72a0aa9a8571173ed048219436
Kirill Ishtuganov <ian574miller@gmail.com>
2024-05-15 12:19:17 +0300

    checkstyle fix

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -18,7 +18,7 @@ public class BaseSchema<T> {
         rules.put(ruleName, rule);
     }
 
-    public BaseSchema<T> required() {
+    public  BaseSchema<T> required() {
         addValidation("required", Objects::nonNull);
         return this;
     }
